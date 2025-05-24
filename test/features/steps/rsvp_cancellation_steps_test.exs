442c7c1f81daf692f19dace99e923c29b91a2909
Micah Woods <mwoods79@users.noreply.github.com>
2025-05-24 16:56:07 -0400

    fix: address credo formatting issue in test file

--- test/features/steps/rsvp_cancellation_steps_test.exs
+++ test/features/steps/rsvp_cancellation_steps_test.exs
@@ -13,36 +13,36 @@ defmodule RsvpCancellationSteps do
   defstep "I have RSVPed to {string}", context do
     huddl_title = List.first(context.args)
     user = context.current_user
-    
-    huddl = 
+
+    huddl =
       Huddl
       |> Ash.Query.filter(title == ^huddl_title)
       |> Ash.read_one!(actor: user)
-    
+
     huddl
     |> Ash.Changeset.for_update(:rsvp, %{user_id: user.id}, actor: user)
     |> Ash.update!()
-    
+
     {:ok, context}
   end
 
   defstep "{string} has RSVPed to {string}", context do
     [email, huddl_title] = context.args
-    
-    user = 
+
+    user =
       User
       |> Ash.Query.filter(email == ^email)
       |> Ash.read_one!(authorize?: false)
-    
-    huddl = 
+
+    huddl =
       Huddl
       |> Ash.Query.filter(title == ^huddl_title)
       |> Ash.read_one!(actor: user)
-    
+
     huddl
     |> Ash.Changeset.for_update(:rsvp, %{user_id: user.id}, actor: user)
     |> Ash.update!()
-    
+
     {:ok, context}
   end
 
@@ -50,29 +50,29 @@ defmodule RsvpCancellationSteps do
   defstep "I visit the {string} huddl page", context do
     huddl_title = List.first(context.args)
     user = context.current_user
-    
-    huddl = 
+
+    huddl =
       Huddl
       |> Ash.Query.filter(title == ^huddl_title)
       |> Ash.Query.load(:group)
       |> Ash.read_one!(actor: user)
-    
-    {:ok, live, _html} = 
+
+    {:ok, live, _html} =
       context.conn
       |> live("/groups/#{huddl.group_id}/huddlz/#{huddl.id}")
-    
+
     {:ok, Map.put(context, :live, live)}
   end
 
   defstep "I click {string}", context do
     button_text = List.first(context.args)
-    
+
     if button_text == "Cancel RSVP" do
       result =
         context.live
         |> element("button", "Cancel RSVP")
         |> render_click()
-      
+
       {:ok, Map.put(context, :last_result, result)}
     else
       # Pass through to other step definitions
