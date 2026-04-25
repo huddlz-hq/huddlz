06cc63df3b6fbdeae2f64aec06431219e9f3a5f2
Micah Woods <mwoods79@users.noreply.github.com>
2026-04-25 12:16:43 -0400

    feat(api): expose HuddlAttendee.by_huddl
    Add base AshJsonApi/AshGraphql extensions to HuddlAttendee. Expose
    the by_huddl read action via GET /api/json/huddl_attendees/by_huddl
    and the huddlAttendees GraphQL query. The existing policy restricts
    the result set to attendees and group owners/organizers.

---
+++ test/huddlz_web/api/json/huddl_attendee_test.exs
@@ -0,0 +1,26 @@
+defmodule HuddlzWeb.Api.Json.HuddlAttendeeTest do
+  use HuddlzWeb.ApiCase, async: true
+
+  describe "GET /api/json/huddl_attendees/by_huddl" do
+    test "organizer can see attendees of a huddl in their group", %{conn: conn} do
+      owner = generate(user())
+      g = generate(group(owner_id: owner.id, is_public: true, actor: owner))
+      h = generate(huddl(group_id: g.id, creator_id: owner.id, actor: owner))
+
+      member = generate(user())
+
+      h
+      |> Ash.Changeset.for_update(:rsvp, %{}, actor: member)
+      |> Ash.update!()
+
+      conn =
+        conn
+        |> authenticated_conn(owner)
+        |> get("/api/json/huddl_attendees/by_huddl?huddl_id=#{h.id}")
+
+      assert %{"data" => data} = json_response(conn, 200)
+      assert is_list(data)
+      assert data != []
+    end
+  end
+end
