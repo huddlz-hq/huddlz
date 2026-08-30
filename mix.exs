0718762e2f1416cb24eba20761c5f4e68356467e
Micah Woods <mwoods79@users.noreply.github.com>
2026-08-29 20:10:44 -0400

    fix(email): include Mailgun multipart dependency

--- mix.exs
+++ mix.exs
@@ -111,6 +111,7 @@ defmodule Huddlz.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.16"},
+      {:multipart, "~> 0.4"},
       {:ical, "~> 2.0"},
       {:req, "~> 0.5"},
       {:req_s3, "~> 0.2"},
